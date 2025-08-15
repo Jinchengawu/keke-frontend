3f747fcf2282999a8c55ab825ad011bc4d6202fc
zhanyuan.wang@ximalaya.com <dreamworks.cnn@gmail.com>
2025-08-15 22:45:22 +0800

    初始化keke-frontend

---
+++ apps/web/src/utils/log.ts
@@ -0,0 +1,53 @@
+import { Currency, TradeType, UnifiedCurrency } from '@pancakeswap/swap-sdk-core'
+
+import { logger } from './datadog'
+
+export const logTx = ({ account, hash, chainId }: { account: string; hash: string; chainId: number }) => {
+  fetch(`/api/_log/${account}/${chainId}/${hash}`)
+}
+
+export const logSwap = ({
+  input,
+  output,
+  inputAmount,
+  outputAmount,
+  chainId,
+  account,
+  hash,
+  type,
+  tradeType,
+}: {
+  tradeType?: TradeType
+  input: UnifiedCurrency
+  output: UnifiedCurrency
+  inputAmount?: string
+  outputAmount?: string
+  chainId: number
+  account: string
+  hash: `0x${string}`
+  type:
+    | 'V2Swap'
+    | 'SmartSwap'
+    | 'StableSwap'
+    | 'MarketMakerSwap'
+    | 'V3SmartSwap'
+    | 'UniversalRouter'
+    | 'X'
+    | 'X-Filled'
+    | 'SolanaSwap'
+}) => {
+  try {
+    logger.info(type, {
+      tradeType,
+      inputAddress: input.isToken ? input.address.toLowerCase() : input.symbol,
+      outputAddress: output.isToken ? output.address.toLowerCase() : output.symbol,
+      inputAmount,
+      outputAmount,
+      account,
+      hash,
+      chainId,
+    })
+  } catch (error) {
+    //
+  }
+}
