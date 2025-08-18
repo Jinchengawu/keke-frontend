67b07ec8f147aa0560134849091515b1c0b95ce1
zhanyuan.wang@ximalaya.com <dreamworks.cnn@gmail.com>
2025-08-18 16:51:09 +0800

    feat: 更新.gitignore以排除.next/目录，并在next.config.mjs中添加作者信息和图像域名配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
+/*
+ * @Author: dreamworks.cnn@gmail.com
+ * @Date: 2025-08-18 10:14:33
+ * @LastEditors: dreamworks.cnn@gmail.com
+ * @LastEditTime: 2025-08-18 14:46:13
+ * @FilePath: /keke-frontend/next.config.mjs
+ * @Description: 
+ * 
+ * Copyright (c) 2025 by ${git_name_email}, All Rights Reserved. 
+ */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ["commons/*"],
+    images: {
+        domains: ['images.unsplash.com'],
+    },
     env: {
         AUTH_MSG: process.env.AUTH_MSG || 'Sign this message to authenticate with Poseidon Pay: <timestamp>',
         POSEIDON_PAY_CONTRACT: process.env.POSEIDON_PAY_CONTRACT || '0x0000000000000000000000000000000000000000',
